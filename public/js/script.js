f8d824177ced498b60623c3d7328f2c587d06ac6
kitchenphones <abdulazammohammed2000@gmail.com>
2024-06-28 12:37:38 -0500

    Initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -26,7 +26,7 @@ document.getElementById('entryForm').addEventListener('submit', async (e) => {
             marketName: document.getElementById('marketName').value
         };
 
-        const response = await fetch('http://your-ec2-instance-public-dns:3000/api/entry', {
+        const response = await fetch('http://ec2-34-235-140-38.compute-1.amazonaws.com:3000/api/entry', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ document.getElementById('soldForm').addEventListener('submit', async (e) => {
         date: document.getElementById('soldDate').value,
     };
 
-    const response = await fetch('http://your-ec2-instance-public-dns:3000/api/sold', {
+    const response = await fetch('http://ec2-34-235-140-38.compute-1.amazonaws.com:3000/api/sold', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
